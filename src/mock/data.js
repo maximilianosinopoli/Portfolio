9f94835d00eb64276ccac295d64f0e70ea703613
Maximiliano Sinopoli <m_sinopoli@hotmail.com>
2021-02-09 19:50:02 +0000

    Portfolio

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,46 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Maximiliano Sinopoli | JS Developer', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: "I'm",
+  name: 'Maximiliano Sinopoli',
+  subtitle: 'a <Front End Developer/>',
+  cta: 'Know more',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'about.jpg',
+  paragraphOne: "I'm a computer science student and a Front End Developer specialised in Javascript technologies based in London, UK.",
+  paragraphTwo: 'I stand out for being an extremely curious person, with a proactive personality and a passion for learning new and exciting things. I love challenges that require logic and critical thinking to overcome.',
+  paragraphThree: "Recently I've done a programming bootcamp over 300 hours. I've learned technologies like React, NodeJS, Express, MongoDB, Mongoose, MySQL, Hooks, HTML and CSS. I like also travel, photography, reading, outdoor sports and learn new languages.",
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+    img: 'recipe.png',
+    title: 'Recipe App',
+    info: 'The app grabs recipes from an external API and allows users to search for specific recipes.',
+    info2: 'Technologies: Javascript, React, Hook, CSS',
+    url: 'https://compassionate-bhabha-585153.netlify.app/',
+    repo: 'https://github.com/maximilianosinopoli/Recipe-App', // if no repo, the button will not show up
+  }
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Would you like to work with me? Awesome!',
+  btn: "Let's talk!",
+  email: 'm_sinopoli@hotmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +49,23 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/maxisinopoli-react/',
+    },
+    {
+      id: nanoid(),
+      name: 'instagram',
+      url: 'https://www.instagram.com/colosinopoli/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/maximilianosinopoli',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
